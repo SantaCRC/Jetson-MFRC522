e125c835b9744fad4bf51c5d575062145a8d7e49
Fabian <alvarez.fabian@outlook.com>
2022-01-05 21:50:45 -0600

    1.0.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="Jetson_MFRC522",
-    version="0.0.3",
+    version="1.0.0",
     author="Fabian Alvarez",
     author_email="support@fabianalvarez.dev",
     description="A library to integrate the MFRC522 RFID readers with the Jetson family",
