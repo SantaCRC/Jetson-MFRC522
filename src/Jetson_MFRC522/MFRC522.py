fcc9e7ba1e6246977410f9a948e823bee599921a
Fabian Alvarez <35088759+SantaCRC@users.noreply.github.com>
2024-10-10 04:12:21 +0200

    Update MFRC522.py

--- src/Jetson_MFRC522/MFRC522.py
+++ src/Jetson_MFRC522/MFRC522.py
@@ -1,24 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf8 -*-
 #
-#    Copyright 2014,2018 Mario Gomez <mario.gomez@teubi.co>
+#    Copyright 2014,2024 Fabian Alvarez <fabian.alvarez@ieee.org>
 #
-#    This file is part of MFRC522-Python
+#    This file is part of Jetson_MFRC522
 #    MFRC522-Python is a simple Python implementation for
 #    the MFRC522 NFC Card Reader for the Raspberry Pi.
 #
-#    MFRC522-Python is free software: you can redistribute it and/or modify
+#    Jetson_MFRC522 is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Lesser General Public License as published by
 #    the Free Software Foundation, either version 3 of the License, or
 #    (at your option) any later version.
 #
-#    MFRC522-Python is distributed in the hope that it will be useful,
+#    Jetson_MFRC522 is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #    GNU Lesser General Public License for more details.
 #
 #    You should have received a copy of the GNU Lesser General Public License
-#    along with MFRC522-Python.  If not, see <http://www.gnu.org/licenses/>.
+#    along with Jetson_MFRC522.  If not, see <http://www.gnu.org/licenses/>.
 #
 import Jetson.GPIO as GPIO
 import spidev
